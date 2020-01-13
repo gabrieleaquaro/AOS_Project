4898bf0ce3195589dc2069b26a691f78a1b4f434
Giuseppe Massari <joe.massanga@gmail.com>
2020-01-13 14:32:10 +0100

    [Build] Global targets added
    Signed-off-by: Giuseppe Massari <joe.massanga@gmail.com>

--- opencl/bosp.mk
+++ opencl/bosp.mk
@@ -1,1 +1,7 @@
+
+
+samples: samples_opencl
+
+clean_samples: clean_samples_opencl
+
 -include samples/opencl/hellocl/bosp.mk
--- opencv/bosp.mk
+++ opencv/bosp.mk
@@ -1,2 +1,7 @@
 
+
+samples: samples_opencv
+
+clean_samples: clean_samples_opencv
+
 -include opencv/background_learning/bosp.mk
