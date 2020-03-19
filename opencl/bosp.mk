a1ba812d8a736f009cea5071fc8ee08f720cdfb2
Giuseppe Massari <joe.massanga@gmail.com>
2020-03-19 16:45:30 +0100

    [Build/Kconfig] New configuration and building for new samples integration
    Signed-off-by: Giuseppe Massari <joe.massanga@gmail.com>

--- opencl/bosp.mk
+++ opencl/bosp.mk
@@ -7,5 +7,6 @@ samples: samples_opencl
 clean_samples: clean_samples_opencl
 
 -include samples/opencl/hellocl/bosp.mk
+#_include_dont_edit_#
 
 endif
--- opencv/bosp.mk
+++ opencv/bosp.mk
@@ -6,5 +6,6 @@ samples: samples_opencv
 clean_samples: clean_samples_opencv
 
 -include opencv/background_learning/bosp.mk
+#_include_dont_edit_#
 
 endif
