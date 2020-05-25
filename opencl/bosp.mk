6d75a2e92687f695be9b4b739d9366e61490317a
Giuseppe Massari <joe.massanga@gmail.com>
2020-05-25 16:12:02 +0200

    [OpenCL/Hotspot] Added ported sample benchmark from Rodinia
    Signed-off-by: Giuseppe Massari <joe.massanga@gmail.com>

--- opencl/bosp.mk
+++ opencl/bosp.mk
@@ -5,5 +5,6 @@ clean_samples: clean_samples_opencl
 
 -include samples/opencl/hellocl/bosp.mk
 -include samples/opencl/heartwall/bosp.mk
+-include samples/opencl/hotspot/bosp.mk
 #_include_dont_edit_#
 
